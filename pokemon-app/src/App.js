464f3a1f72ccd94455e06e7fd0a1b8f16f6fca30
meglaurie <megan.laurie@edu.sait.ca>
2020-10-06 19:53:03 -0600

    App set up, data from api is fetched

---
+++ pokemon-app/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect }from 'react';
+import { getAllPokemon } from './services/pokemon';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+      async function fetchData () {
+        let response = await getAllPokemon(initialUrl);
+        console.log(response);
+        setNextUrl(response.next);
+        setPrevUrl(response.previous);
+        setLoading(false);
+      }
+      fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+    {loading ? <h1>Loading...</h1> : (
+      <h1>Data is fetched</h1>
+    )}
+    </div>
+  );
+}
+
+export default App;
